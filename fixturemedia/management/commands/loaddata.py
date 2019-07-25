b53d069974e2f0fee76622580f6db9821598c063
Ramil Aglyautdinov <aglyautdinov@gmail.com>
2019-07-25 11:27:45 +0300

    Simplified media fixture path finding, use filefield storage instead of default storage

--- fixturemedia/management/commands/loaddata.py
+++ fixturemedia/management/commands/loaddata.py
@@ -4,8 +4,6 @@ from os.path import join
 import django.core.management.commands.loaddata
 import django.core.serializers
 from django.apps import apps
-from django.conf import settings
-from django.core.files.storage import default_storage
 from django.db.models import signals
 from django.db.models.fields.files import FileField
 
@@ -22,6 +20,11 @@ def models_with_filefields():
 class Command(django.core.management.commands.loaddata.Command):
 
     def load_images_for_signal(self, sender, **kwargs):
+        fixture_paths = self.fixture_dirs
+        fixture_paths = (join(path, 'media') for path in fixture_paths)
+        fixture_paths = [path for path in fixture_paths if isdir(path)]
+        self.fixture_media_paths = fixture_paths
+
         instance = kwargs['instance']
         for field in sender._meta.fields:
             if not isinstance(field, FileField):
@@ -33,7 +36,7 @@ class Command(django.core.management.commands.loaddata.Command):
                 filepath = join(fixture_path, path.name)
                 try:
                     with open(filepath, 'rb') as f:
-                        default_storage.save(path.name, f)
+                        field.storage.save(path.name, f)
                 except file_not_found_error:
                     self.stderr.write("Expected file at {} doesn't exist, skipping".format(filepath))
                     continue
@@ -43,15 +46,4 @@ class Command(django.core.management.commands.loaddata.Command):
         for modelclass in models_with_filefields():
             signals.pre_save.connect(self.load_images_for_signal, sender=modelclass)
 
-        fixture_paths = self.find_fixture_paths()
-        fixture_paths = (join(path, 'media') for path in fixture_paths)
-        fixture_paths = [path for path in fixture_paths if isdir(path)]
-        self.fixture_media_paths = fixture_paths
-
         return super(Command, self).handle(*fixture_labels, **options)
-
-    def find_fixture_paths(self):
-        """Return the full paths to all possible fixture directories."""
-        app_fixtures = [join(config.path, 'fixtures') for config in apps.app_configs.values()]
-
-        return app_fixtures + list(settings.FIXTURE_DIRS)
